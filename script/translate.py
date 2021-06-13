dd661dc0b87bddeb04b820d7358724aa4bde1004
Haleh <www.hhsshhss@gmail.com>
2021-06-13 16:31:35 +0430

    [SCRIPT] add run & install script

---
+++ script/translate.py
@@ -0,0 +1,20 @@
+import subprocess
+import os
+import sys
+
+
+if os.path.exists("./env") == False:
+    subprocess.run("pip install -r requirement.txt")
+    subprocess.run("virtualenv env", shell=True)
+
+    if sys.platform == "win32":
+        os.system("env\\Scripts\\activate")
+    else:
+        subprocess.call("source .env/bin/activate", shell=True, executable="/bin/bash")
+
+
+os.chdir("my_translate_server")
+subprocess.call("start /b python manage.py runserver 5005", shell=True)
+
+os.chdir("../my_translate_front/build")
+subprocess.call("start /b python -m http.server", shell=True)
