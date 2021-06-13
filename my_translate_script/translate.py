348bde890abfdb6c233e6ace539e5892fe66f565
Haleh <www.hhsshhss@gmail.com>
2021-06-13 17:22:18 +0430

    [SCRIPT] add comment

--- my_translate_script/translate.py
+++ my_translate_script/translate.py
@@ -4,17 +4,30 @@ import sys
 
 
 if os.path.exists("./env") == False:
+    print("[create environment ...]")
     subprocess.run("virtualenv env", shell=True)
-    subprocess.run("pip install -r ../../requirement.txt")
 
     if sys.platform == "win32":
+        print("[activate environment ...]")
         os.system("env\\Scripts\\activate")
     else:
+        print("[activate environment ...]")
         subprocess.call("source .env/bin/activate", shell=True, executable="/bin/bash")
 
+    print("[installing requirements ...]")
+    os.system("pip install -r ../requirements.txt")
 
 os.chdir("../my_translate_server")
-subprocess.call("start /b python manage.py runserver 5005", shell=True)
+
+print("[migrate changes ...]")
+os.system("python manage.py makemigrations")
+os.system("python manage.py migrate")
+
+print("[running server ...]")
+os.system("start /b python manage.py runserver 5005")
+
 
 os.chdir("../my_translate_front/build")
-subprocess.call("start /b python -m http.server", shell=True)
+
+print("[running front ...]")
+os.system("start /b python -m http.server")
