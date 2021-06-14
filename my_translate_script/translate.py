af3ecbd6523243ad061f487f274f05e0b764ab5c
bright98 <www.hhsshhss@gmail.com>
2021-06-14 23:24:15 +0430

    [SCRIPT] fix translate.py

--- my_translate_script/translate.py
+++ my_translate_script/translate.py
@@ -3,16 +3,18 @@ import os
 import sys
 
 
-if os.path.exists("./env") == False:
-    print("\n[create environment ...]\n")
-    subprocess.run("virtualenv env", shell=True)
+if not os.path.exists("./env"):
 
-    if sys.platform == "win32":
-        print("\n[activate environment ...]\n")
-        os.system("env\\Scripts\\activate")
-    else:
-        print("\n[activate environment ...]\n")
-        subprocess.call("source .env/bin/activate", shell=True, executable="/bin/bash")
+    # -- envirement --
+    # print("\n[create environment ...]\n")
+    # subprocess.run("virtualenv env", shell=True)
+
+    # if sys.platform == "win32":
+    #     print("\n[activate environment ...]\n")
+    #     os.system("env\\Scripts\\activate")
+    # else:
+    # print("\n[activate environment ...]\n")
+    # subprocess.call("source .env/bin/activate", shell=True, executable="/bin/bash")
 
     print("\n[installing requirements ...]\n")
     os.system("pip install -r ../requirements.txt")
@@ -20,16 +22,22 @@ if os.path.exists("./env") == False:
 os.chdir("../my_translate_server")
 
 print("\n[migrate changes ...]\n")
-os.system("python manage.py makemigrations")
-os.system("python manage.py migrate")
+os.system("python3 manage.py makemigrations")
+os.system("python3 manage.py migrate")
 
 print("\n[running server ...]\n")
-os.system("start /b python manage.py runserver 5005")
+if sys.platform == "win32":
+    os.system("start /b python3 manage.py runserver 5005")
+else:
+    os.system("python3 manage.py runserver 5005 &")
 
 
 os.chdir("../my_translate_front/build")
 
 print("\n[running front ...]\n")
-os.system("start /b python -m http.server")
+if sys.platform == "win32":
+    os.system("start /b python3 -m http.server")
+else:
+    os.system("python3 -m http.server 9000 &")
 
 print("\n---[successfully done ...]---\n")
