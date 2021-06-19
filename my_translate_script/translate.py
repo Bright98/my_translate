0f519d00c24d6d04c2bd09ecee6bfdd4b92d1151
Haleh <www.hhsshhss@gmail.com>
2021-06-19 10:28:52 +0430

    [SCRIPT] fix try except

--- my_translate_script/translate.py
+++ my_translate_script/translate.py
@@ -3,28 +3,34 @@ import sys
 
 
 if not os.path.exists("./env"):
-    print("\n[installing requirements ...]\n")
+    print("\n[ installing requirements ... ]\n")
     os.system("pip install -r ../requirements.txt")
+else:
+    print("\n[ boro amoo jan aval ye venv ba esm env bessaz... :) ]\n")
+    exit()
 
 os.chdir("../my_translate_server")
 
-print("\n[migrate changes ...]\n")
+print("\n[ migrate changes ... ]\n")
 os.system("python manage.py makemigrations")
 os.system("python manage.py migrate")
 
-print("\n[running server ...]\n")
-if sys.platform == "win32":
-    os.system("start /b python manage.py runserver 5005")
-else:
-    os.system("python manage.py runserver 5005 &")
 
+try:
+    print("\n[ running server ... ]\n")
+    if sys.platform == "win32":
+        os.system("start /b python manage.py runserver 5005")
+    else:
+        os.system("python manage.py runserver 5005 &")
+except:
+    print("\n[ nemidoonam moshkel az system khodete be ma che... ]\n")
+    exit()
 
 os.chdir("../my_translate_front")
-
-print("\n[running front ...]\n")
+print("\n[ running front ... ]\n")
 if sys.platform == "win32":
     os.system("start /b python -m http.server")
 else:
     os.system("python -m http.server &")
 
-print("\n---[successfully done ...]---\n")
+print("\n---[ successfully done ... ]---\n")
